e4c4ccc3c8131755780abf051bf4bd5bcd521a88
ChenZhaoYu <790348264@qq.com>
2022-12-07 16:10:21 +0800

    chore: add styled-components and react-is

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Button } from 'antd';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 const App = () => {
   return (
     <div className="p-4">
+      <Title>Hello CodeSandbox</Title>
       <Button>按钮</Button>
     </div>
   );
