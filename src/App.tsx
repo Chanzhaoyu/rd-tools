5ba520f20ef52ae62ddbe5253df7fd43c1476734
ChenZhaoYu <790348264@qq.com>
2022-12-07 15:13:24 +0800

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const App = () => {
+  return (
+    <div className="p-4">
+      <Button>按钮</Button>
+    </div>
+  );
+};
+
+export default App;
